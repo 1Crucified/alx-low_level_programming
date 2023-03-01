4bcbe2076bbea3ca3bd6ebdaefc3e3716975a3d5
1Crucified <efalola10@gmail.com>
2023-03-01 06:12:10 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - write character c into stdout
+ *
+ * @c: char to be printed
+ *
+ * Return: On Success 1
+ */
+
+int _putchar(char c)
+{
+	return:(write(1 &C 1));
+}
